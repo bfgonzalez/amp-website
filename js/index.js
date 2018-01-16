c6a4ea2211405a27b89de6b53de44fe2549c6840
Bianca Gonzalez <33680528+bfgonzalez@users.noreply.github.com>
2018-01-16 13:13:50 +0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#sidenav").hide();
+  
+  // scroll effect
+  $("a[href^='#']").on("click", function() {
+    var the_id = $(this).attr("href");
+  
+      $("html, body").animate({
+        scrollTop:$(the_id).offset().top
+      }, "slow");
+
+    return false;});
+  
+  // toggles dropdown menu on click
+  $("#menu").on("click", function() {
+    $("#sidenav").fadeToggle();  
+  });
+  
+  $("a[href^='#']").on("click", function() {
+    $("#sidenav").fadeOut();
+  });
+
+
+});
