273c4c744dc72dfcd17605139507fc56a385d936
Bianca Gonzalez <33680528+bfgonzalez@users.noreply.github.com>
2018-01-31 23:04:34 +0800

    made intitial layout for events page
    upcoming event + countdown timer

--- js/index.js
+++ js/index.js
@@ -126,4 +126,35 @@ $(document).ready(function() {
   $("li").on("click", function () {
     $(this).toggleClass("pink-highlight");
   });
+
+  // countdown timer for upcoming event 
+  // date we're counting down to
+  var countDownDate = new Date("May 4, 2018 19:00:00").getTime();
+  
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    $("#countdown").text("Time until event: " + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ");
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        $("#countdown").text("Event is over");
+    }
+  }, 1000);
+  
 });				
