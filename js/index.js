17d944a7428251fc6a2bf49d4b6fcb894c6f1d30
Bianca Gonzalez <33680528+bfgonzalez@users.noreply.github.com>
2018-02-11 13:38:44 +0800

    layout changes
    Merged "amplugged" and "news" into one page named "latest" and removed scroll animation which was unecessary

--- js/index.js
+++ js/index.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
   $("#sidenav").hide();
   
-  // scroll effect
-  $("a[href^='#']").on("click", function() {
-    var the_id = $(this).attr("href");
-  
-      $("html, body").animate({
-        scrollTop:$(the_id).offset().top
-      }, "slow");
-
-    return false;});
-  
   // toggles dropdown menu on click
   $("#menu").on("click", function() {
     $("#sidenav").fadeToggle();  
