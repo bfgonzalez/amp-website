2eed043a77772c1fe24d55786de245b5eb2d6ded
bfgonzalez <bfgonzalez142@gmail.com>
2018-07-15 18:35:54 +0800

    design overhaul for homepage

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-  $("#sidenav").hide();
-  
-  // toggles dropdown menu on click
-  $("#menu").on("click", function() {
-    $("#sidenav").fadeToggle();  
+  // changes navbar color on scroll for better contrast
+  var scroll_start = 0;
+  var start_change = $("#about");
+  var offset = start_change.offset();
+  $(document).scroll(function () {
+    scroll_start = $(this).scrollTop();
+    if(scroll_start > offset.top) {
+      $("#nav-wrapper").css("background", "#E74265");
+      $("#nav-wrapper").css("transition", "400ms");
+    } else {
+      $("#nav-wrapper").css("background", "transparent");
+    }
   });
-  
-  // closes sidenav once link has been clicked
-  $("a[href^='#']").on("click", function() {
-    $("#sidenav").fadeOut();
-  });
-  
+
   // modal pop-up for each band
   $("#band-1").on("click", function() {
     $("#modal-1").modal({
@@ -60,7 +62,7 @@ $(document).ready(function() {
     });
   });
 
-  // modal pop-up for each soloist roster 
+  // modal pop-up for each soloist roster
   $("#vocals").on("click", function() {
     $("#modal-1").modal({
       fadeDuration: 100
@@ -102,7 +104,7 @@ $(document).ready(function() {
       fadeDuration: 100
     });
 	});
-  
+
   // opens & closes more-info on click of roster member (soloists)
   $(".more-info").hide();
   $("li").on("click", function() {
@@ -117,36 +119,36 @@ $(document).ready(function() {
     $(this).not(".band-members > li").toggleClass("pink-highlight");
   });
 
-  // countdown timer for upcoming event 
+  // countdown timer for upcoming event
   // date we're counting down to
   var countDownDate = new Date("May 4, 2018 19:00:00").getTime();
-  
+
   // Update the count down every 1 second
   var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
+
     // Find the distance between now an the count down date
     var distance = countDownDate - now;
-    
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     // Output the result in an element with id="demo"
     $("#days").text(days + "d");
     $("#hours").text(hours + "h");
     $("#minutes").text(minutes + "m");
     $("#seconds").text(seconds + "s");
-    
-    // If the count down is over, write some text 
+
+    // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
         $("#countdown").text("Event is over");
     }
   }, 1000);
-  
-});				
+
+});
